0d98a03a2c8ce160beeb42d997dc39fa6cb0aca5
mohamedaburizwan <mdaburizwan@gmail.com>
2023-02-25 09:45:03 +0530

    Changes commit

--- src/main/java/testSuites/TestLegalEntity.java
+++ src/main/java/testSuites/TestLegalEntity.java
@@ -10,14 +10,8 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import base.BaseHooks;
 
 public class TestLegalEntity extends BaseHooks {
@@ -151,6 +145,7 @@ public class TestLegalEntity extends BaseHooks {
        System.out.println(errorFieldText);
        assertEquals(genericText, "Review the following fields");
        assertEquals(errorFieldText, "Name");
+       System.out.println("Added");
 }
 
 }
